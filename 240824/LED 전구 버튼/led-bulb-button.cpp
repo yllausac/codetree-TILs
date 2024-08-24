f5b7fe3e04df9b69541a9997bb21018b4c192c76
yllausac <83061357+yllausac@users.noreply.github.com>
2024-08-24 16:11:24 +0900

    Title: LED 전구 버튼, Time: 1025ms, Memory: 0MB, Status: Time Limit Exceed - CodeTree

--- 240824/LED 전구 버튼/led-bulb-button.cpp
+++ 240824/LED 전구 버튼/led-bulb-button.cpp
@@ -7,31 +7,25 @@ int main() {
     int N, B;
     cin >> N >> B;
     vector<int> bulbs(N);
-    vector<int> next_bulbs(N);
-    for (int i=0; i<N; i++){
-       cin >> bulbs[i];
-    }
-    next_bulbs = bulbs;
 
-    for (int i=0; i<B; i++){
-        for (int j=0; j<N; j++){
-            if (j == 0){
-                if (bulbs[N-1] == 1){
-                    if (bulbs[j] == 0) next_bulbs[j] = 1;
-                    else next_bulbs[j] = 0;
-                }
-            } 
-            else if (bulbs[j-1] == 1){
-                if (bulbs[j] == 0) next_bulbs[j] = 1;
-                else next_bulbs[j] = 0;
+    for (int i = 0; i < N; i++) {
+        cin >> bulbs[i];
+    }
+
+    for (int i = 0; i < B; i++) {
+        vector<int> next_bulbs = bulbs;
+        for (int j = 0; j < N; j++) {
+            int left = (j == 0) ? bulbs[N - 1] : bulbs[j - 1];
+            if (left == 1) {
+                next_bulbs[j] = 1 - bulbs[j];
             }
         }
         bulbs = next_bulbs;
     }
-    
-    for (int i=0; i<N; i++){
+
+    for (int i = 0; i < N; i++) {
         cout << bulbs[i] << endl;
     }
-    
+
     return 0;
 }
