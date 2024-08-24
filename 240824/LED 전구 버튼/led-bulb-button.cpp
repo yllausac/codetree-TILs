b4f31451b56f0c4c48e875bcf6872a77a40ca07d
yllausac <83061357+yllausac@users.noreply.github.com>
2024-08-24 16:03:11 +0900

    Title: LED 전구 버튼, Time: 1080ms, Memory: 0MB, Status: Time Limit Exceed - CodeTree

---
+++ 240824/LED 전구 버튼/led-bulb-button.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int N, B;
+    cin >> N >> B;
+    vector<int> bulbs(N);
+    vector<int> next_bulbs(N);
+    for (int i=0; i<N; i++){
+       cin >> bulbs[i];
+    }
+    next_bulbs = bulbs;
+
+    for (int i=0; i<B; i++){
+        for (int j=0; j<N; j++){
+            if (j == 0 && bulbs[N-1] == 1){
+                if (bulbs[j] == 0) next_bulbs[j] = 1;
+                else next_bulbs[j] = 0;
+            } 
+            else if (bulbs[j-1] == 1){
+                if (bulbs[j] == 0) next_bulbs[j] = 1;
+                else next_bulbs[j] = 0;
+            }
+        }
+        bulbs = next_bulbs;
+    }
+    
+    for (int i=0; i<N; i++){
+        cout << bulbs[i] << endl;
+    }
+    
+    return 0;
+}
